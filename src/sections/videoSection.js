4816b51175dafda454de622ca0b5700849244beb
Sarah86 <sarah@DESKTOP-VI58DL9.localdomain>
2020-09-27 08:44:13 +0200

    update gatsby-cle

--- src/sections/videoSection.js
+++ src/sections/videoSection.js
@@ -7,21 +7,22 @@ import { device } from "../components/devices.js"
 const StyledSection = styled.div`
     height: calc(100vh - 40px);
     position: relative;
-    @media ${device.largeDesktop}{
-    }
 `
 
-const VideoSection = () => (
+const VideoSection = () => {
 
-    <StyledSection id="home" className="bg-white z-index-1">
-        <FullWidth style={{height: '100%'}}>
-            <Video 
-                url="https://www.youtube.com/watch?v=S4cVfwV4Wqw" 
-                width="100vw" 
-                height="100%"
-                backgroundColor=" rgba(0, 0, 0, 0.3)" />
-        </FullWidth>
-    </StyledSection>
-)
+    return(
+
+        <StyledSection id="home" className="bg-white z-index-1">
+            <FullWidth style={{height: '100%'}}>
+                <Video 
+                    url="https://www.youtube.com/watch?v=S4cVfwV4Wqw" 
+                    width="100vw" 
+                    height="100%"
+                    backgroundColor=" rgba(0, 0, 0, 0.3)" />
+            </FullWidth>
+        </StyledSection>
+    )
+}
 
 export default VideoSection
