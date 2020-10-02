84d44561c805ebe2c30ac85c3586ae5ff337bf57
Sarah86 <sarah@DESKTOP-VI58DL9.localdomain>
2020-10-02 18:04:53 +0200

    git build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/mnt/c/Users/sarah/OneDrive/Documentos/PROJETOS/__DONE/LAGOON/lagoon-website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/mnt/c/Users/sarah/OneDrive/Documentos/PROJETOS/__DONE/LAGOON/lagoon-website/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/mnt/c/Users/sarah/OneDrive/Documentos/PROJETOS/__DONE/LAGOON/lagoon-website/src/pages/index.js")))
 }
